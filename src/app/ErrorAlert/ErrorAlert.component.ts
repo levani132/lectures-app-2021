6c390198baa28586bd4e5ed4acdaaa735c5a295a
posiashvili <posiashvili@bog.ge>
2021-05-20 12:54:09 +0400

    1
    1

---
+++ src/app/ErrorAlert/ErrorAlert.component.ts
@@ -0,0 +1,11 @@
+
+import { Component } from "@angular/core";
+
+ 
+@Component({
+    selector:'bg-ErrorAlert',
+    templateUrl:'./ErrorAlert.component.html',
+    styleUrls: ['./ErrorAlert.component.scss']
+}
+)
+export class ErrorAlertComponent {}
