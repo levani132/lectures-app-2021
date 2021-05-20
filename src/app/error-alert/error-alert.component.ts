ad3b8e73c137dc610982a6359d87f309c1acc116
nkupatadze <nkupatadze@bog.ge>
2021-05-21 02:18:50 +0400

    nino-seminar-1

---
+++ src/app/error-alert/error-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bg-error-alert',
+  templateUrl: './error-alert.component.html',
+  styleUrls: ['./error-alert.component.scss']
+})
+export class ErrorAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
