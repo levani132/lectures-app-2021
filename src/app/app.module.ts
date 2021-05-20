8d03b4ebfb49a90a1f731c584f1a9eafc0db99bb
nkupatadze <nkupatadze@bog.ge>
2021-05-21 01:36:09 +0400

    ninoK-seminar-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ShellComponent } from './shell/shell.component';
-
+import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     ShellComponent
   ],
   imports: [
-    BrowserModule
+
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -5,11 +5,34 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss']
 })
-export class ShellComponent implements OnInit {
+export class ShellComponent  {
+  username = '';
+  text = '';
+  smthng = "appear";
+  counter = 0;
+  
+  myFun(){
+    this.username = '';
+  }
+  rame(){
+    if(this.text == ''){
+      this.text = "გამაქრე";
+      this.smthng = "hide";
+    }
+    else {
+      this.text = '';
+      this.smthng = "appear";
+    }
+    this.counter +=1;
+    // if(this.counter > 5){
+
+    // }
+
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
+
+
