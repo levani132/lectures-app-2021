2ab53ff31d4f1d746480f60ea4f853bfdeea5614
Alex <asurg17@freeuni.edu.ge>
2021-05-25 10:39:56 +0400

    as-seminar-4-done

---
+++ src/app/popup/popup.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[bgPopup]'
+})
+export class PopupDirective {
+
+  @HostBinding('class.open')
+  open = false;
+
+  constructor(private elementRef: ElementRef<HTMLElement>) { }
+
+  @HostListener('document:click', ['$event'])
+  clickListener(event: Event){
+    // @ts-ignore
+    if(this.elementRef.nativeElement.contains(event.target)){
+      this.open = !this.open;
+    }else{
+      this.open = false;
+    }
+  }
+
+}
