49c6649f325a27b47850862c3c68f2d77544f03b
L.I <lashaiakobadze98@gmail.com>
2021-05-25 12:23:04 +0400

    Last

--- src/app/popup/popup.directive.ts
+++ src/app/popup/popup.directive.ts
@@ -1,15 +1,16 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[bgPopup]'
 })
 export class PopupDirective {
+
+  constructor(private elementRef: ElementRef<HTMLElement>) { }
   @HostBinding('class.open') isOpen = false;
 
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
-  }
 
-  constructor() { }
+  @HostListener('document:click', ['$event']) toggleOpen($event) {
+    this.isOpen = this.elementRef.nativeElement.contains($event.target) ? !this.isOpen : false;
+  }
 
 }
