0af5d30f2fa6f2e31e6ca662819b904e41634cd6
David <dvddevadze@gmail.com>
2021-06-01 00:01:19 +0400

    first half

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Observer, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { SeminarService } from './seminar.service';
 
@@ -11,8 +13,17 @@ export class AppComponent implements OnInit {
   showCongratulations = false;
 
   constructor(private seminarService: SeminarService) {}
-
+  
   ngOnInit() {
     // TODO: აქ დაწერეთ თქვენი interval observable-ის კოდი.
+    function timeout(time: number) {
+      return new Observable<number>((observer: Observer<number>) => {
+        setTimeout(() => {
+          observer.next(time);
+        }, time);
+      });
+    }
+    const subs = timeout(500).pipe(map((value,index) => 'dato ' + value + ' dato'));
+    subs.subscribe(value => console.log(value));
   }
 }
